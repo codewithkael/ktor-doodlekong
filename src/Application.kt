2c1de6a3a495441c82b48d1ddd797e1928b6e94f
CodeWithKael <mr.ghelyon@gmail.com>
2022-01-27 12:05:04 +0330

    aa

--- src/Application.kt
+++ src/Application.kt
@@ -7,12 +7,18 @@ import com.plcoding.routes.getRoomsRoute
 import com.plcoding.routes.joinRoomRoute
 import com.plcoding.session.DrawingSession
 import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.gson.*
+import io.ktor.response.*
+import io.ktor.request.*
 import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.websocket.*
+import io.ktor.http.cio.websocket.*
 import io.ktor.sessions.*
 import io.ktor.util.*
-import io.ktor.websocket.*
+import java.time.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -20,12 +26,13 @@ val server = DrawingServer()
 val gson = Gson()
 
 @Suppress("unused") // Referenced in application.conf
-fun Application.module() {
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
     install(Sessions) {
         cookie<DrawingSession>("SESSION")
     }
     intercept(ApplicationCallPipeline.Features) {
-        if (call.sessions.get<DrawingSession>() == null) {
+        if(call.sessions.get<DrawingSession>() == null) {
             val clientId = call.parameters["client_id"] ?: ""
             call.sessions.set(DrawingSession(clientId, generateNonce()))
         }
